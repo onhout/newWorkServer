b1c5795a16fede686049f3130cb3047b528a900e
Peter Liu <onhout@gmail.com>
2016-12-05 16:10:31 -0800

    uncomment the php upgrades

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -4,7 +4,7 @@
 function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
 
 #add repos
-sudo add-apt-repository -y ppa:ondrej/php5-5.6
+sudo add-apt-repository -y ppa:ondrej/php
 sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
 #update
@@ -32,8 +32,10 @@ fi
 npm install -g bower
 
 #write hosts
+sudo su
 sudo echo "192.168.50.233	prop-db-active" >> /etc/hosts
 sudo echo "192.168.120.253	active-directory" >> /etc/hosts
+exit
 
 #test php version
 php56version=5.6
@@ -43,7 +45,7 @@ phpver=`php -v |grep -Eow '^PHP [^ ]+' |gawk '{ print $2 }'`
 if version_gt $php56version $phpver; then
      echo "$php56version is greater than installed version! ($phpver)!"
      echo "installing PHP$php56version..."
-     #sudo apt-get install -y php5.6 php5.6-cli php5.6-common php5.6-curl php5.6-dev php5.6-gd php5.6-intl php5.6-json php5.6-ldap php5.6-mbstring php5.6-mcrypt php5.6-memcache php5.6-memcached php5.6-mysql php5.6-pgsql php5.6-readline php5.6-sqlite php5.6-xml php5.6-xsl libzip4 php5.6-zip libapache2-mod-php5
+     sudo apt-get install -y php5.6 php5.6-cli php5.6-common php5.6-curl php5.6-dev php5.6-gd php5.6-intl php5.6-json php5.6-ldap php5.6-mbstring php5.6-mcrypt php5.6-memcache php5.6-memcached php5.6-mysql php5.6-pgsql php5.6-readline php5.6-sqlite php5.6-xml php5.6-xsl libzip4 php5.6-zip libapache2-mod-php5
      sudo a2dismod php5.5
      sudo a2enmod php5.6
      sudo service apache2 restart
